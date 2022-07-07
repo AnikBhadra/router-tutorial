7ca7b214791b6a3159f14817b7da7d492e4f4039
anik <anikbhadra01@gmail.com>
2022-07-07 20:57:50 +0530

    navigating programmatically

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import { Navbar } from './components/Navbar'
+import { OrderSummary } from './components/OrderSummary'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
+        <Route path='order-summary' element={<OrderSummary />}></Route>
       </Routes>
     </div>
   )
