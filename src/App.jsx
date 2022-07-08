0d328a2d41fc55634a7b1325d4db9c82dd9c4b2a
anik <anikbhadra01@gmail.com>
2022-07-08 10:31:12 +0530

    dynamic routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import NoMatch from './components/NoMatch'
 import Products from './components/Products'
 import FeaturedProducts from './components/FeaturedProducts'
 import NewProduct from './components/NewProduct'
+import Users from './components/Users'
+import UserDetails from './components/UserDetails'
+import Admin from './components/Admin'
 
 function App() {
 
@@ -19,14 +22,19 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='about' element={<About />} />
         <Route path='order-summary' element={<OrderSummary />}></Route>
-        <Route path='*' element={<NoMatch />} > </Route>
-
         <Route path='products' element={<Products />} >
           <Route index element={<FeaturedProducts />} />
 
           <Route path='featured' element={<FeaturedProducts />} />
           <Route path='new' element={<NewProduct />} />
         </Route>
+
+        <Route path='users' element={<Users />}>
+          <Route path=':userId' element={<UserDetails />} />
+          <Route path='admin' element={<Admin />} />
+        </Route >
+        <Route path='*' element={<NoMatch />} />
+
       </Routes>
     </div>
   )
